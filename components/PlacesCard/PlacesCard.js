977b6bdf3b1a4d58234f39dcced48e92db94740c
Saad Baradan <saadbaradan@gmail.com>
2019-07-16 15:38:22 -0600

    Pass down trip to placesCard and display place names
    Co-authored-by: Theo Bean <beantheo@gmail.com>

--- components/PlacesCard/PlacesCard.js
+++ components/PlacesCard/PlacesCard.js
@@ -2,11 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function PlacesCard(props) {
+  const { navigation } = props;
+  const trip = navigation.getParam('trip', 'loading')
   return (
     <View>
-      {/* <Text>{props.place.name}</Text>
-      <Text>{props.place.weather.weather}</Text> */}
-      <Text>Hi</Text>
+      {trip.places.map(place => {
+        return(<Text key={place.name}>{place.name}</Text>)
+      })}
     </View>
   );
 }
