588fdca3e1507a07e8a3aa35895fc03b45582b30
Saad Baradan <saadbaradan@gmail.com>
2019-07-17 11:39:14 -0600

    Add legs to places card and style legs information
    Co-authored-by: Theo Bean <beantheo@gmail.com>

--- components/PlacesCard/PlacesCard.js
+++ components/PlacesCard/PlacesCard.js
@@ -2,23 +2,54 @@ import React from "react";
 import { StyleSheet, Text, View, ImageBackground, TouchableOpacity} from "react-native";
 import { weatherList } from '../../assets/backgroundImages';
 
+
 export default function PlacesCard(props) {
   const { navigation } = props;
   const trip = navigation.getParam('trip', 'loading')
 
+  hoursToNextPlace = hours => {
+    var sign = hours < 0 ? "-" : "";
+    var hour = Math.floor(Math.abs(hours));
+    var min = Math.floor((Math.abs(hours) * 60) % 60);
+    return sign + (hour < 10 ? "0" : "") + hour + ":" + (min < 10 ? "0" : "") + min;
+   }
+
   return (
     <View style={styles.view}>
-      {trip.places.map(place => {
+      {trip.places.map((place, index) => {
         let weather = place.weather.summary
-        return(
-          <ImageBackground imageStyle={{ borderRadius: 20}} source={{uri: weatherList[weather]}} key={place.name} style={styles.image} >
-            <TouchableOpacity>
-              <Text style={styles.name}>{place.name}</Text>
-              <Text style={styles.weather}>{place.weather.temperature + '\u2109'}</Text>
-              <Text style={styles.summary}>{place.weather.summary}</Text>
-            </TouchableOpacity>
-          </ImageBackground>
-          )
+        
+        if(trip.legs[index]) {
+          return(
+            <View>
+            <ImageBackground imageStyle={{ borderRadius: 20}} source={{uri: weatherList[weather]}} key={place.name} style={styles.image} >
+              <TouchableOpacity>
+                <Text style={styles.name}>{place.name}</Text>
+                <Text style={styles.weather}>{place.weather.temperature + '\u2109'}</Text>
+                <Text style={styles.summary}>{place.weather.summary}</Text>
+              </TouchableOpacity>
+            </ImageBackground>
+            <View style={styles.legs}>
+              <Text style={styles.arrow}>{'\u2193'}</Text>
+                <Text style={styles.distance}>{trip.legs[index].distance}</Text>
+                <Text style={styles.hours}>{hoursToNextPlace(trip.legs[index].duration_in_hours) + 'hrs'}</Text>
+            </View>
+            </View>
+            )
+        } else {
+          return(
+            <View>
+            <ImageBackground imageStyle={{ borderRadius: 20}} source={{uri: weatherList[weather]}} key={place.name} style={styles.image} >
+              <TouchableOpacity>
+                <Text style={styles.name}>{place.name}</Text>
+                <Text style={styles.weather}>{place.weather.temperature + '\u2109'}</Text>
+                <Text style={styles.summary}>{place.weather.summary}</Text>
+              </TouchableOpacity>
+            </ImageBackground>
+            </View>
+            )
+        }
+        
       })}
     </View>
   );
@@ -54,7 +85,19 @@ const styles = StyleSheet.create({
     marginBottom: 0,
     marginLeft: 'auto',
   },
-  view: {
-   
+  legs: {
+   flexDirection: 'row',
+   width: '100%',
+   justifyContent: 'space-around',
+  },
+  arrow: {
+    fontSize: 30,
+  },
+  distance: {
+    fontSize: 30,
+  },
+  hours: {
+    fontSize: 30,
+  
   }
 })
